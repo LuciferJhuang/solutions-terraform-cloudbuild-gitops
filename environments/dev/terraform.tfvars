bd224f8a1608ff0c773e748b25d819aab702fb36
Sumit L <asksnupma@gmail.com>
2024-05-09 07:05:38 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="projecta-418002"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "projecta-418002-tfstate"
     prefix = "env/prod"
   }
 }
