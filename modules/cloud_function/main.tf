2401d47d4cf57fe633630cb5b4a9bf9cd3d719dc
Sandeep Agarwal <pensande@gmail.com>
2025-03-26 15:53:33 +0000

    bug fixing 3

--- modules/cloud_function/main.tf
+++ modules/cloud_function/main.tf
@@ -49,14 +49,14 @@ resource "google_cloudfunctions2_function" "function" {
     }
   }
 
-  dynamic "event_trigger" {
-    for_each = var.triggers == null ? [] : var.triggers
-    content {
-      event_type  = event_trigger.value.event_type
-      pubsub_topic = event_trigger.value.event_type == "google.cloud.pubsub.topic.v1.messagePublished" ? event_trigger.value.resource : null
-      event_filters {
-        attribute = event_trigger.value.event_type == "google.cloud.storage.object.v1.finalized" ? "bucket" : null
-        value     = event_trigger.value.event_type == "google.cloud.storage.object.v1.finalized" ? event_trigger.value.resource : null
+  event_trigger {
+    event_type    = var.triggers[0].event_type
+    pubsub_topic  = var.triggers[0].event_type == "google.cloud.pubsub.topic.v1.messagePublished" ? var.triggers[0].resource : null
+    dynamic "event_filters" {
+      for_each = var.triggers[0].event_type == "google.cloud.storage.object.v1.finalized" ? var.triggers : []
+      content {
+        attribute = "bucket"
+        value     = event_filters.value.resource
       }
     }
   }
