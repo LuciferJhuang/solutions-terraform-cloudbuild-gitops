33372392b915681c9f5f29c81c9623bc882446ae
LuciferJhuang <108084494+LuciferJhuang@users.noreply.github.com>
2025-04-17 22:32:53 +0800

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -38,3 +38,14 @@ module "firewall" {
   project = "${var.project}"
   subnet  = "${module.vpc.subnet}"
 }
+
+module "im-workspace" {
+ source = "terraform-google-modules/bootstrap/google//modules/im_cloudbuild_workspace"
+ version = "~> 7.0"
+
+ project_id = e6j2-training
+ deployment_id = cloudbuild-terraform-demo
+ im_deployment_repo_uri = https://github.com/LuciferJhuang/solutions-terraform-cloudbuild-gitops
+
+ github_personal_access_token = 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
+}
